d6ec7c5a8b5a3991410327e8eab6e7883e8155eb
Yukiii <2856525619@qq.com>
2024-12-10 16:20:48 +0800

    [init] 初始化项目

---
+++ src/main/java/com/xuanxuan/warningdetect/WarningDetectApplication.java
@@ -0,0 +1,13 @@
+package com.xuanxuan.warningdetect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarningDetectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarningDetectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xuanxuan/warningdetect/WarningDetectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuanxuan.warningdetect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarningDetectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
