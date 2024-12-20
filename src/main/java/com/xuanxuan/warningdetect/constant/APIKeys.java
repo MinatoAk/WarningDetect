87df0fba8a73d38722131bafd8c4d8e40c4faa66
Yukiii <2856525619@qq.com>
2024-12-20 11:48:32 +0800

    [modify] 删除 apiKeys

--- src/main/java/com/xuanxuan/warningdetect/constant/APIKeys.java
+++ src/main/java/com/xuanxuan/warningdetect/constant/APIKeys.java
@@ -5,5 +5,5 @@ public interface APIKeys {
      * todo: apiKey 需要从智谱开放平台获取
      * https://open.bigmodel.cn/usercenter/proj-mgmt/apikeys
      */
-    String ZHIPU_AI_KEY = "ff02923fabe413d1c28d4fd6d88d9551.yAmA57lQ8lUl4MYJ";
+    String ZHIPU_AI_KEY = "";
 }
